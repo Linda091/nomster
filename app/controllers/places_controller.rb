085eebc556849ff78f33527b206710ea3e287202
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-02-07 16:17:06 +0000

    added styling to user authentication pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+     redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -39,9 +43,9 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   def destroy
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed', status : :forbidden
+      return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
