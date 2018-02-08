317d494e4891ef9b7e597e21b958fb29d0921951
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-02-08 20:36:55 +0000

    added css styling to validation lession - 27

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -37,7 +37,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+     redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
